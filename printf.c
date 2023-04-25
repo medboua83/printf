843ef2f81ae63177495a655fe9b1920bcfb2cd3e
medboua83 <bouazza.medyt2@gmail.com>
2023-04-25 04:54:18 -0700

    default

--- printf.c
+++ printf.c
@@ -3,13 +3,12 @@
 
 int _printf(const char *format, ...)
 {
-    va_list arg;
-    int done;
+	va_list arg;
+	int done;
 
-    va_start(arg, format);
-    done = vfprintf(stdout, format, arg);
-    va_end(arg);
-
-    return done;
+	va_start(arg, format);
+	done = vfprintf(stdout, format, arg);
+	va_end(arg);
+	return (done);
 }
 
